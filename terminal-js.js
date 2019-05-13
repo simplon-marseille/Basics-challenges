91df5f83b54888be0784c89aaaa308ecfc2dd5f1
simplon-marseille <acardnicolas@hotmail.com>
2019-05-13 20:30:59 +0200

    first commit

---
+++ terminal-js.js
@@ -0,0 +1,161 @@
+//Bienvenue à ce premier challenge.
+//Vous afficherez les resultats dans le terminal avec la commande:
+// 'node <nom du ficher>'
+// Pour que les resultats soient visible dans le terminal, vous utiliserez
+// la fonction 'console.log'
+// ex: console.log('simplon');
+// ex: console.log(fonction());
+// ex: console.log(nom de la variable);
+// Lorque vous avez validé un exercice, mettez le en commentaire !!
+
+
+//Variables et opérateurs
+//Déclarer deux variables avec comme valeur 4 et 5
+//Créez une autre variable contenant l'addition des deux et l'afficher.
+
+
+
+
+
+//Additionnez ces deux termes et stockez le résultat dans une variable
+const num1 = "4";
+const num2 = "5";
+//TODO
+
+
+
+
+
+
+
+
+
+//Les tableaux
+//Déclarez un tableau avec 5 villes
+//Afficher la ville à l'indice 3
+//Ajouter une ville au tableau
+//Supprimer la ville à l'indice 2
+
+const villes = [];
+//TODO
+
+
+
+
+//Les boucles
+//
+//Boucler sur le tableau 'array1' afin de monter chaque
+//éléments au carré et afficher le tableau.
+
+const array1 = [2, 4, 8];
+//TODO
+
+
+
+//les conditions
+//Déclarez une variable "age" avec la valeur de votre choix.
+//En utilisant les conditions 'if' et 'else' affichez :
+//peut voter si 'age' est supérieur ou égal à 18
+//ne peut pas voter si 'age' est inférieur à 18
+
+
+
+//Créez deux variables initialisées à 3 et 4.
+//Créez une fonction qui retourne l'addition de ces deux valeurs.
+
+
+const add = (num1, num2) => {
+//TODO
+};
+
+
+
+
+
+//Créez une fonction qui prend en paramètre la valeur i. La fonction doit
+//retourner la valeur i au carré et l'afficher.
+
+const square = (number) => {
+  //TODO
+};
+
+
+
+// En utilisant la fonction 'map', mettre la chaine de
+// caractère en majuscule.
+const beatles = ["paul", "john", "ringo", "george"];
+
+//Créez un tableau nommé tab dont le premier
+//élément est -2, le deuxième 3 et le troisième 4
+//Créez une fonction qui soustrait chaque éléments du tableau si celui-ci est supérieur à 0.
+
+const soustrait = (tableau) => {
+ //TODO
+};
+
+
+
+
+
+
+//Créez une fonction qui renvoie la somme de chaque éléments du
+//tableau si celui-ci est supérieur à 0.
+const array2 = [-2, 4, -5, 3, 6];
+const addPositiveNumber = (tableau) => {
+ //TODO
+};
+
+
+
+
+//Créez une fonction qui prend un parametre 'r' et qui
+//renvoie le périmetre d'un cercle.
+const périmetreCercle = (r) => {
+  //TODO
+};
+
+//Créez une fonction qui met la premiere lettre d'un mot en majuscule.
+//Le reste du mot doit être en minuscule.
+//Le mot est "SimPloN"
+const capitalize = (word) => {
+  //TODO
+};
+
+
+
+//Odd or Even
+//Créez un fonction qui prend un nombre en parametre et qui renvoie :
+//paire sir le nombre est pair
+//impaire si le nombre est impair
+//Tips : jetez un coup d'oeil à l'opérateur '%' dans la doc.
+
+
+
+//Mot inversé
+//créez une fonction qui prend en parametre une chaine de caractère.
+//Cette fonction renvoie la chaine de caractère inversée.
+//ex: reverseWord('simplon') ==> 'nolpmis'
+//Aide : join(), split(), reverse()
+const reverseWord = (word) => {
+  //TODO
+};
+
+
+//créez une focntion qui renvoie la somme de tableau
+const nombres = ["3", "5", "6", "2"];
+
+
+
+// const fs = require('fs')
+
+// // Data which will write in a file.
+// let data = "Learning how to write in a file."
+
+// // Write data in 'Output.txt' .
+// fs.writeFile('Output.txt', data, (err) => {
+
+//     // In case of a error throw err.
+//     if (err) throw err;
+// })
+
+
