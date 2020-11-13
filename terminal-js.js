c29df6fc326155ae688f163710a164d7f0c2effa
simplon-marseille <acardnicolas@hotmail.com>
2020-11-13 08:33:53 +0100

    mise à jour

--- terminal-js.js
+++ terminal-js.js
@@ -81,22 +81,6 @@ const promo = [12, 13, 17, 3, 14, 18];
 //Et pense à commit ton projet
 
 
-// La suite de Fibonacci est une suite d'entiers dans laquelle chaque terme est la somme des deux 
-// termes qui le précèdent. Elle commence généralement par les termes 0 et 1 (parfois 1 et 1) et ses 
-// premiers termes sont : 0, 1, 1, 2, 3, 5, 8, 13, 21, etc...
-// Code un algorithme qui calcul les 50 premiers termes de la suite.
-
-
-
-// Créer un jeu simple de devinette de nombre. Le jeu choisit aléatoirement un nombre 
-// entre 1 et 100, puis il met le joueur au défi de le deviner en 10 tentatives maxi.À chaque tour, le 
-// joueur doit être informé s'il a deviné ou non le bon nombre — si ce n'est pas le cas, le jeu lui 
-// indique si son estimation est trop basse ou trop élevée.Le jeu se termine quand le joueur a deviné le nombre mystère, ou 
-// s'il a épuisé ses 10 chances. À la fin du jeu, le joueur a la possibilité de débuter une nouvelle 
-// partie.
-
-//Bonus : Le jeu doit également rappeler au joueur les 
-// nombres déjà proposés
 
 
 //----------------- Fonctions -------------------
@@ -128,14 +112,6 @@ const beatles = ["paul", "john", "ringo", "george"];
 //Créez un tableau nommé tab dont le premier
 //élément est 2, le deuxième 3 et le troisième 4.
 //Créez une fonction qui renvoit la somme des éléments du tableau.
-
-
-//Tri à bulle
-//Classé les éléments du tableau suivant par ordre croissant.
-//Affichez le tableau classé.
-
-const aTrier = [3, 2, 6, 4, 8, 10];
-
 
 
 
@@ -187,6 +163,42 @@ const reverseWord = (word) => {
 const nombres = ["3", "5", "6", "2"];
 
 
+
+
+
+
+
+//-----------------------------EXO SUP-------------------------------------
+
+
+
+
+
+
+
 //Coder la fonction puissance qui prend en paramètre deux arguments (nombre et puissance).
 //La fonction devra renvoyer le nombre monté à la puissance
-//L'argument 'puissance' valoir 0, 1 ou être negatif et bien sûr positif.
+//L'argument 'puissance' peut valoir 0, 1 ou être negatif et bien sûr positif.
+
+
+// Créer un jeu simple de devinette de nombre. Le jeu choisit aléatoirement un nombre 
+// entre 1 et 100, puis il met le joueur au défi de le deviner en 10 tentatives maxi.À chaque tour, le 
+// joueur doit être informé s'il a deviné ou non le bon nombre — si ce n'est pas le cas, le jeu lui 
+// indique si son estimation est trop basse ou trop élevée.Le jeu se termine quand le joueur a deviné le nombre mystère, ou 
+// s'il a épuisé ses 10 chances. À la fin du jeu, le joueur a la possibilité de débuter une nouvelle 
+// partie.
+//Bonus : Le jeu doit également rappeler au joueur les 
+// nombres déjà proposés
+
+
+//Tri à bulle
+//Classé les éléments du tableau suivant par ordre croissant.
+//Affichez le tableau classé.
+
+const aTrier = [3, 2, 6, 4, 8, 10];
+
+
+// La suite de Fibonacci est une suite d'entiers dans laquelle chaque terme est la somme des deux 
+// termes qui le précèdent. Elle commence généralement par les termes 0 et 1 (parfois 1 et 1) et ses 
+// premiers termes sont : 0, 1, 1, 2, 3, 5, 8, 13, 21, etc...
+// Code un algorithme qui calcul les 50 premiers termes de la suite.
